aaa5809807f5b96d28f7b9ce0d5c4b59ca99c726
vijay kumar navulla <vijaykumarnavulla@gmail.com>
2019-06-17 12:10:46 +0530

    deleting unnecessary documentaion, move to sub branches.

--- src/app/modules/context-menu/quick-date-picker/quick-date-picker.module.spec.ts
+++
@@ -1,8 +0,0 @@
-import { QuickDatePickerModule } from './quick-date-picker.module';
-
-
-describe('QuickDatePickerModule', () => {
-  it('should work', () => {
-    expect(new QuickDatePickerModule()).toBeDefined();
-  });
-});
