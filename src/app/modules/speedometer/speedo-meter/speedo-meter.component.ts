27d14177fe6c9aff85f31fa30b4eb6b6e944598b
vijay kumar navulla <vijaykumarnavulla@gmail.com>
2019-06-19 19:02:41 +0530

    updating the speedometer same as edge-ui.

--- src/app/modules/speedometer/speedo-meter/speedo-meter.component.ts
+++ src/app/modules/speedometer/speedo-meter/speedo-meter.component.ts
@@ -1,23 +1,63 @@
-import { Component} from '@angular/core';
+import { Component, Input, ElementRef, Renderer2, ViewChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-speedo-meter',
   templateUrl: './speedo-meter.component.html',
   styleUrls: [ './speedo-meter.component.css' ]
 })
-export class SpeedoMeterComponent {
-  private speedScore:number = 0; //min : 0; max: 100
-  private readingSpeed:number;
-  private niddleSpeed:number;
-  constructor() { }
+export class SpeedoMeterComponent implements OnInit {
+  public speedScore:number = 0; //min : 0; max: 100
+  public readingSpeed:number;
+  public niddleSpeed:number;
+  @ViewChild('xotbSpeedometer') xotbSpeedometer: ElementRef;
+  @ViewChild('xotbSpeedometerWheelColor') xotbSpeedometerWheelColor: ElementRef;
+  @ViewChild('xotbSpeedometerWheelColorDiv') xotbSpeedometerWheelColorDiv: ElementRef;
+  styleClass = ".class1{}";
+  pseudoStyle = ".xOTB-speedometerCentreCircle:after{background: red;}";
+
+  @Input() set speedoMeterColor(color: string) {
+    this.renderer.setStyle(this.xotbSpeedometer.nativeElement, 'border-top-color', 'transparent');
+    this.renderer.setStyle(this.xotbSpeedometer.nativeElement, 'border-right-colorr', 'transparent');
+    this.renderer.setStyle(this.xotbSpeedometer.nativeElement, 'border-bottom-color', color);
+    this.renderer.setStyle(this.xotbSpeedometer.nativeElement, 'border-left-color', color);
+  }
+
+  @Input() set wheelColor(color: string) {
+    this.renderer.setStyle(this.xotbSpeedometerWheelColor.nativeElement, 'border-bottom-width', '110px');
+    this.renderer.setStyle(this.xotbSpeedometerWheelColor.nativeElement, 'border-bottom-style', 'solid');
+    this.renderer.setStyle(this.xotbSpeedometerWheelColor.nativeElement, 'border-bottom-color', color);
+    const classStyle = this.renderer.createText(this.styleClass);
+    const pseudoElementStyle = this.renderer.createText(this.pseudoStyle);
+
+    // Insert the previously defined style into a new <style> element
+    const newStyleElement = this.renderer.createElement('style');
+    // this.renderer.appendChild(newStyleElement, classStyle);
+    this.renderer.appendChild(newStyleElement, pseudoElementStyle);
+    this.renderer.appendChild(this.xotbSpeedometerWheelColorDiv.nativeElement, newStyleElement);
+  }
+
+
+
+  @Input() max:number = 100;
+
+  constructor(private renderer: Renderer2) { }
 
   updateSpeed(){
-    this.readingSpeed = Math.round(this.speedScore * 180/100)-45;
-    this.niddleSpeed = Math.round(this.speedScore * 180/100)-90;
+    this.readingSpeed = Math.round(this.speedScore * 180/this.max)-45;
+    this.niddleSpeed = Math.round(this.speedScore * 180/this.max)-90;
   }
 
   getSpeed(speed){
     this.speedScore = speed;
     this.updateSpeed();
+  }
+
+  ngOnChanges(){
+
+  }
+
+  ngOnInit(){
+
+
   }
 }
