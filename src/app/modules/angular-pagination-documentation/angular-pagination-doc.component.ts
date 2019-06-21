cdc0cbc8a00bc7842a8a4346d29bd8095e30c551
Dinesh Rajput <rajputd@HCL.COM>
2019-06-21 16:33:33 +0530

    documentation move in component form

---
+++ src/app/modules/angular-pagination-documentation/angular-pagination-doc.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+//import { HttpClient, HttpHeaders  } from '@angular/common/http';
+
+@Component({
+  selector: 'app-pagination-doc',
+  templateUrl: './angular-pagination-doc.component.html',
+  styleUrls: [ './styles.css','./angular-pagination-doc.component.css' ]
+})
+
+export class PagDocComponent implements OnInit {  
+    constructor() {   
+
+     }
+
+     ngOnInit() {}
+}
